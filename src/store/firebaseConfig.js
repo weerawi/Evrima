90e808e99b7368aee2089d19a11136965ee28a9a
Weerawi <weerawikramatharindu@gmail.com>
2023-07-09 18:44:39 +0530

    now google sign in option is popup working

--- src/store/firebaseConfig.js
+++ src/store/firebaseConfig.js
@@ -1,24 +1,43 @@
-import firebase from 'firebase/app';
-// import {getAuth , GoogleAuthProvider , signInWithGoogle} from 'firebase/auth';
-// import App from '../App';
-import 'firebase/compat/auth';
+ 
 
-
+import firebase from "firebase/app"
+import "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD-zjXyGJ6PfDkMLGc-VY4Q_s_T5j8dFSY",
-    authDomain: "evrima-1685701666983.firebaseapp.com",
-    projectId: "evrima-1685701666983",
-    storageBucket: "evrima-1685701666983.appspot.com",
-    messagingSenderId: "842744481983",
-    appId: "1:842744481983:web:e004b03dc6e8c774d79e0a",
-    measurementId: "G-5V0CBF1KB1"
-  };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-//  const auth = getAuth(App); 
-
-export default firebase;
+  apiKey: "AIzaSyD-zjXyGJ6PfDkMLGc-VY4Q_s_T5j8dFSY",
+  authDomain: "evrima-1685701666983.firebaseapp.com",
+  projectId: "evrima-1685701666983",
+  storageBucket: "evrima-1685701666983.appspot.com",
+  messagingSenderId: "842744481983",
+  appId: "1:842744481983:web:e004b03dc6e8c774d79e0a",
+  measurementId: "G-5V0CBF1KB1"
+};
 
  
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(provider)
+    .then((result) => {
+      const name = result.user.displayName;
+      const email = result.user.email;
+      const profilePic = result.user.photoURL; 
+
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("profilePic", profilePic);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}; 
+
+
+
+
+
+
