a3fe71364f2f1fb7c42f2aa5f907743da1194931
Weerawi <weerawikramatharindu@gmail.com>
2023-07-09 17:23:56 +0530

    adding sign up button styling

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -80,13 +80,17 @@ const AuthForm = () => {
         <h1 class="text-center text-gray-800 font-bold text-2xl">{isLogin ? 'Login' : 'Sign Up'}</h1>
         <form  onSubmit={submitHandler}>
           
+        {isLogin &&
 
+          (
 
-          <div class="my-5 border-b text-center">
-            <div style={{background :  'transparent' } }
-              class="leading-none px-2 inline-block text-lg text-gray-200 tracking-wide font-medium transform translate-y-1/2"
+          <div  >
+
+          <div class="  border-b text-center">
+            <div 
+              class="leading-none px-2 inline-block text-lg bg-cyan-800 text-gray-200 border-2 p-1 rounded-full  tracking-wide font-medium transform translate-y-1/2"
             >
-              With
+              Sign With
             </div>
           </div>
 
@@ -94,13 +98,18 @@ const AuthForm = () => {
 
 
           <div class="mt-6 flex flex-row  ">
-              <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
+
+            {/* Google */}
+              <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-cyan-400 hover:bg-gray-100 focus:bg-blue-50 active:bg-blue-100"
+              >
                   <div class="  flex items-center   justify-center">
                       <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" class="  w-5" alt="google logo"/>
                        
                   </div>
               </button>
-              <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
+
+            {/* Github */}
+              <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-cyan-400 hover:bg-gray-100 focus:bg-blue-50 active:bg-blue-100">
                   <div class="  flex items-center   justify-center">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="  w-5 text-gray-700" viewBox="0 0 16 16">
                       <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
@@ -108,8 +117,10 @@ const AuthForm = () => {
                        
                   </div>
               </button>
+
+              {/* Facebook */}
               <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 
-                            hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
+                            hover:border-cyan-400 hover:bg-gray-100 focus:bg-blue-50 active:bg-blue-100">
                   <div class="  flex items-center   justify-center">
                         <img src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" class="  w-5" alt="Facebook logo"/>
                        
@@ -119,14 +130,16 @@ const AuthForm = () => {
 
 
 
-          <div class="my-5 border-b text-center">
-            <div style={{background :  'transparent' } }
-              class="leading-none px-2 inline-block text-lg text-gray-200 tracking-wide font-medium transform translate-y-1/2"
+          <div class=" border-b text-center">
+            <div 
+              class="leading-none px-2 inline-block text-lg text-gray-200 border-2 p-1 rounded-full bg-cyan-800 tracking-wide font-medium transform translate-y-1/2"
             >
               Or 
             </div>
           </div>
 
+          </div>
+          )}
 
           <div className=" mb-2">
             <label className='block text-gray-200 font-bold mb-2' htmlFor='email'>Your Email</label>
