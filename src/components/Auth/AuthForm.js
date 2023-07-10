0ff3e76be945f767c2cbe62785fe5817d25307c6
Weerawi <weerawikramatharindu@gmail.com>
2023-07-10 10:08:52 +0530

    Change to calling two sign in method seperatley

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,15 +1,17 @@
 
 import { useState, useRef, useContext } from 'react';
 import { useHistory } from 'react-router-dom'; 
-import AuthContext from '../../store/auth-context'; 
+import AuthContext from '../../store/auth-context';  
+import useSignIn from "../../store/firebaseConfig";
+
 
 const AuthForm = () => {
   const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
-  const authCtx = useContext(AuthContext);
-
+  const authCtx = useContext(AuthContext); 
+  
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -17,6 +19,10 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const {signInWithGoogle} = useSignIn();   
+  const {signInWithFacebook} = useSignIn();   
+   
+  
   const submitHandler = (event) => {
     event.preventDefault();
 
@@ -33,6 +39,8 @@ const AuthForm = () => {
     } else {
       url =
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD-zjXyGJ6PfDkMLGc-VY4Q_s_T5j8dFSY';
+
+        
     }
     fetch(url, {
       method: 'POST',
@@ -48,7 +56,7 @@ const AuthForm = () => {
       .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          return res.json();
+          return res.json(); 
         } else {
           return res.json().then((data) => {
             let errorMessage = 'Authentication failed!';
@@ -64,9 +72,10 @@ const AuthForm = () => {
       .then((data) => {
         const expirationTime = new Date(
           new Date().getTime() + +data.expiresIn * 1000
-        );
-        authCtx.login(data.idToken, expirationTime.toISOString());
-        history.replace('/');
+        ); 
+        
+        authCtx.login(data.idToken, expirationTime.toISOString() );
+        history.replace('/ ');
       })
       .catch((err) => {
         alert(err.message);
@@ -101,7 +110,7 @@ const AuthForm = () => {
 
             {/* Google */}
               <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-cyan-400 hover:bg-gray-100 focus:bg-blue-50 active:bg-blue-100"
-              >
+              onClick={signInWithGoogle}>
                   <div class="  flex items-center   justify-center">
                       <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" class="  w-5" alt="google logo"/>
                        
@@ -109,7 +118,8 @@ const AuthForm = () => {
               </button>
 
             {/* Github */}
-              <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-cyan-400 hover:bg-gray-100 focus:bg-blue-50 active:bg-blue-100">
+              <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-cyan-400 hover:bg-gray-100 focus:bg-blue-50 active:bg-blue-100"
+              >
                   <div class="  flex items-center   justify-center">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="  w-5 text-gray-700" viewBox="0 0 16 16">
                       <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
@@ -120,7 +130,8 @@ const AuthForm = () => {
 
               {/* Facebook */}
               <button class="group h-10 bg-white mx-auto px-6 border-2 border-gray-300 rounded-full transition duration-300 
-                            hover:border-cyan-400 hover:bg-gray-100 focus:bg-blue-50 active:bg-blue-100">
+                            hover:border-cyan-400 hover:bg-gray-100 focus:bg-blue-50 active:bg-blue-100"
+                            onClick={signInWithFacebook}>
                   <div class="  flex items-center   justify-center">
                         <img src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" class="  w-5" alt="Facebook logo"/>
                        
