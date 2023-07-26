628b8004dab24540ce59aff84613efe53066ba5b
Weerawi <weerawikramatharindu@gmail.com>
2023-07-26 09:29:45 +0530

    images added and updated

---
+++ frontend/components/HeroBanner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeroBanner = () => {
+
+    const divStyle = {
+        backgroundImage: `url("../assets/bgvideo" )`,
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundSize: 'cover',
+        // position: 'relative', 
+        maxWidth: '100%',
+      };
+
+  return (
+    <div>HeroBanner</div>
+  )
+}
+
+export default HeroBanner
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,25 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 
+import HeroBanner from "@/components/HeroBanner";
+import About from "./About";
+
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24
         
        `}
     >
+
+
+      <HeroBanner/> 
+
+
+
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -53,6 +63,8 @@ export default function Home() {
 
         image
       </div>
+
+      <About/>
 
       
     </main>
