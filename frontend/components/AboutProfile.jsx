76b02938cf2d9805987ad9505b7c651b3fce597d
Weerawi <weerawikramatharindu@gmail.com>
2023-07-26 09:29:28 +0530

    about page working check

---
+++ frontend/components/AboutProfile.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React from 'react'
+
+const AboutProfile = ({image,name,position}) => {
+  return (
+    <div class="space-y-4 text-center">
+        <Image class="w-64 h-64 mx-auto object-cover rounded-xl md:w-48 md:h-64 lg:w-64 lg:h-80 shadow-lg shadow-white transition-transform duration-300 transform-gpu hover:scale-90" 
+        src={image} alt="man" loading="lazy" width="1000" height="667" />
+        <div>
+        <h4 class="text-2xl capitalize text-gray-300 font-medium">{name}</h4>
+        <span class="block text-sm text-gray-100">{position}</span>
+        </div>
+    </div>
+  )
+}
+
+export default AboutProfile
---
+++ frontend/pages/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'; 
+import AboutProfile from '@/components/AboutProfile';
+import { Image } from '@/constants/Images';
+
+const About = () => {
+  return (
+    <div  className=" "> 
+      <div style={{backgroundColor: 'rgba(139, 172, 170, 0.3)'}}  className="container py-20  mx-auto px-6 md:px-12 xl:px-32">
+        <div className="mb-16 text-center">
+          <h2 className="mb-4 text-center text-gray-200 text-2xl   font-bold md:text-4xl">Our Team</h2>
+          <p className="text-gray-300 font-medium lg:w-8/12 lg:mx-auto">Meet our exceptional team, driving innovation and excellence.</p>
+        </div>
+        <div className="grid gap-12 items-center md:grid-cols-3">
+
+          <AboutProfile name={'thari nav'} image={Image.Tharindu} position={"front end"}/>
+          <AboutProfile name={'chamuditha'} image={Image.Chamuditha} position={"front end"}/>
+          <AboutProfile name={'samitha'} image={Image.Samitha} position={"front end"}/>
+          <AboutProfile name={'ushara'} image={Image.Ushara} position={"front end"}/>
+          <AboutProfile name={'imashi'} image={Image.Imashi} position={"front end"}/>
+           
+        </div>
+      </div>
+    </div>
+  );
+};
+// fhgdfchjwjd
+export default About;
