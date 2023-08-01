29f87d6e134f5cab042c8fdb5e6961b92ebaa046
Weerawi <weerawikramatharindu@gmail.com>
2023-08-01 11:05:21 +0530

    some files deleted

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -3,6 +3,7 @@ import Tharindu from '../assets/tharindu.jpg';
 import Samitha from '../assets/samitha.jpg';
 import Ushara from '../assets/ushara.jpg';
 import Imashi from '../assets/imashi.jpg';
+import Evrima from '../assets/evrimalogo.png'
 
 
-export const Image = {Chamuditha ,Tharindu ,Samitha ,Ushara ,Imashi };
+export const Image = {Chamuditha ,Tharindu ,Samitha ,Ushara ,Imashi, Evrima };
