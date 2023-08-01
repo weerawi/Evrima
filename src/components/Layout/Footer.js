6cba9c188dba2abe6234fce6377349c0972e7e68
Weerawi <weerawikramatharindu@gmail.com>
2023-08-01 11:06:54 +0530

    icons install and footer create& add wrapper lyout

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,101 +1,16 @@
-// import React, { useEffect } from 'react';
-
  
-// const Footer = () => {
-
-//   useEffect(() => {
-//     const preloader = document.querySelector("#preloader");
-//     if (preloader) {
-//       const removePreloader = () => {
-//         preloader.remove();
-//       };
-  
-//       window.addEventListener("load", removePreloader);
-  
-//       return () => {
-//         window.removeEventListener("load", removePreloader);
-//       };
-//     }
-//   }, []);
-
-//   return (
-
-//     <>
-
-//       <div id="preloader" className='z-100' > 
-//         <div class="flex items-center justify-center w-full h-full">
-//           <div class="flex justify-center items-center space-x-1 text-sm text-gray-700">
-            
-//                 <svg fill='none' class="w-6 h-6 animate-spin" viewBox="0 0 32 32" xmlns='http://www.w3.org/2000/svg'>
-//                   <path clip-rule='evenodd'
-//                     d='M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z'
-//                     fill='currentColor' fill-rule='evenodd' />
-//                 </svg>
-
-            
-//             <div>Loading ...</div>
-//           </div>
-//         </div>
-      
-//        </div>
-    
-//     <footer  className="bg-cyan-900 py-4">
+import React from "react";
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
  
-//       <div className=" relative">
-//         <svg 
-//           className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-cyan-900 "
-//           preserveAspectRatio="none"
-//           viewBox="0 0 1440 54"
-//         >
-//           <path
-//             fill="currentColor"
-//             d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
-//           />
-//         </svg>
-
-//         <div className="bg-cyan-900">
-//           <div className="max-w-2xl mx-auto text-white py-10">
-//             <div className="text-center">
-//               <h3 className="text-3xl mb-3"> Let's keep in touch!</h3>
-//               <p>Unleash the Power of Discovery</p>
-               
-//             </div> 
-//             <div className="mt-8 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-200">
-//               <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy; Evrima Team 2023</p>
-//               <div className="order-1 md:order-2">
-//                 <span className="px-2">About us</span>
-//                 <span className="px-2 border-l">Contact us</span>
-//                 <span className="px-2 border-l">Privacy Policy</span>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </footer>
-//     </>
-
-//   );
-// };
-
-// export default Footer;
-
-
-
-
-import React, { useEffect, useState } from 'react';
+import { Link } from "@chakra-ui/react";
+import Wrapper from "../Wrapper";
 
 const Footer = () => {
-   
+    return (
+        <footer className="bg-black text-white pt-8 pb-3 relative">
 
-  return (
-    <>
-       
-
-      <footer  className="bg-cyan-900 py-4">
- 
-      <div className=" relative">
         <svg 
-          className="absolute top-0 w-full h-6 -mt-10 sm:-mt-12 sm:h-16 text-cyan-900 "
+          className="absolute top-0 w-full h-5 -mt-4  lg:-mt-8 lg:h-8   sm:-mt-6 sm:h-6 text-black "
           preserveAspectRatio="none"
           viewBox="0 0 1440 54"
         >
@@ -105,31 +20,141 @@ const Footer = () => {
           />
         </svg>
 
-        <div className="bg-cyan-900">
-          <div className="max-w-2xl mx-auto text-white py-5">
-            <div className="text-center">
-              <h3 className="text-3xl mb-3"> Let's keep in touch!</h3>
-              <p>Unleash the Power of Discovery</p>
-              <div className="order-1 md:order-2 py-2">
-                <span className="px-2">About us</span>
-                <span className="px-2 border-l">Contact us</span>
-                <span className="px-2 border-l">Privacy Policy</span>
-              </div>
-              <p className="order-2 md:order-1 mt-4 md:mt-0 border-t  "> &copy; Evrima Team 2023</p>
-               
-            </div> 
-            <div className="mt-3 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-200">
-              <p className="order-2 md:order-1 mt-4 md:mt-0"> dmmy1 </p>
-              <div className="order-1 md:order-2">
-                dummy2
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-    </>
-  );
+            <Wrapper className="flex justify-between flex-col items-center md:flex-row gap-[50px] md:gap-0">
+                {/* LEFT START */}
+                <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] flex-col md:flex-row  ">
+                    {/* MENU START */}
+                    <div className="flex flex-col gap-3 shrink-0">
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            Find a store
+                        </div>
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            become a partner
+                        </div>
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            sign up for email
+                        </div>
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            send us feedback
+                        </div>
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            student discount
+                        </div>
+                    </div>
+                    {/* MENU END */}
+
+                    {/* NORMAL MENU START */}
+                    <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] shrink-0">
+                        {/* MENU START */}
+                        <div className="flex flex-col gap-3">
+                            <div className="font-oswald font-medium uppercase text-sm">
+                                get help
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Order Status
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Delivery
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Returns
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Payment Options
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Contact Us
+                            </div>
+                        </div>
+                        {/* MENU END */}
+
+                        {/* MENU START */}
+                        <div className="flex flex-col gap-3">
+                            <div className="font-oswald font-medium uppercase text-sm">
+                                About nike
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                News
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Careers
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Investors
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Sustainability
+                            </div>
+                        </div>
+                        {/* MENU END */}
+                    </div>
+                    {/* NORMAL MENU END */}
+                </div>
+                {/* LEFT END */}
+
+                {/* RIGHT START */}
+                <div className="flex gap-4 justify-center md:justify-start">
+                    <div
+                        onClick={() =>
+                            window.open("https://facebook.com", "_blank")
+                        }
+                        className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center
+                         text-black hover:bg-[#1877F2]/[0.8] cursor-pointer"
+                    >
+                        <FaFacebookF size={20} />
+                    </div>
+                    <Link
+                        href="https://twitter.com"
+                        className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center
+                         text-black hover:bg-[#1DA1F2]/[0.8] cursor-pointer"
+                    >
+                        <FaTwitter size={20} />
+                    </Link>
+                    <div 
+                      onClick={() =>
+                        window.open("https://youtube.com", "_blank")
+                      }
+                      className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center
+                       text-black hover:bg-[#FF0000]/[0.8] cursor-pointer">
+                        <FaYoutube size={20} />
+                    </div>
+                    <div 
+                      onClick={() =>
+                        window.open("https://instagram.com", "_blank")
+                      }
+                      className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center
+                       text-black hover:bg-[#d62976]/[0.8] cursor-pointer">
+                        <FaInstagram size={20} />
+                    </div>
+                </div>
+                {/* RIGHT END */}
+            </Wrapper>
+            <Wrapper className="flex justify-between mt-10 flex-col md:flex-row gap-[10px] md:gap-0">
+                {/* LEFT START */}
+                <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center md:text-left">
+                    © 2023 Nike, Inc. All Rights Reserved
+                </div>
+                {/* LEFT END */}
+
+                {/* RIGHT START */}
+                <div className="flex gap-2 md:gap-5 text-center md:text-left flex-wrap justify-center">
+                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                        Guides
+                    </div>
+                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                        Terms of Sale
+                    </div>
+                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                        Terms of Use
+                    </div>
+                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                        Privacy Policy
+                    </div>
+                </div>
+                {/* RIGHT END */}
+            </Wrapper>
+        </footer>
+    );
 };
 
 export default Footer;
