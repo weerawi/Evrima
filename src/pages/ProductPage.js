888a5a79796fd4d99881aead501c4128cce558ee
Weerawi <weerawikramatharindu@gmail.com>
2023-09-26 16:35:16 +0530

    productdetails page design created level 1

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,7 @@
 import { Button } from '@chakra-ui/react';
 import React from 'react'
 import { useLocation } from 'react-router-dom';
-// import "./productPage.css";
+import "./productPage.css";
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import AOS from "aos";
@@ -22,104 +22,99 @@ export const ProductPage = ( ) => {
 //   const { name, image, price, views } = location.state || {};
 
 
+
    
    
 
   return (
-    <div className='  h-[150vh] md:h-[100vh]    py-5 md:pb-1 pb-10 '>
+    <div className='   h-[100vh]    py-10 '>
 
-
-        <div className='w-full   text-gray-800 '>
-            
-                <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px] mt-12">
-                    {/* PRODUCT LEFT SIDE IMAGE CAROUSAL */}
-                    <div  data-aos="fade-right"
-                         className="  items-center justify-center w-full md:w-auto flex md:flex-[.7] max-w-[700px] lg:max-w-full mx-auto lg:mx-0 ">
  
-                                    {/*  {image}   */}
-                      <img style={{
-                        
-                        backdropFilter: 'blur(25px)', 
-                        background:  'rgba(139, 172, 170, 0.5)',
-                        border :  '1px solid rgba(255, 255, 255, 0.45) ',   
-                        backgroundSize: 'cover', 
-                        }}
-                         src={product.image_url} alt="Product" className="p-5 shadow-md shadow-white w-auto h-100 object-cover  my-4 border-2 rounded-xl" />
-   
-                    </div>
+        <div className='details'>
+            
 
-                    {/* PRODUCT RIGHT SIDE DETAILS */}
-                    <div   data-aos="fade-left"  style={{
-                        
-                        backdropFilter: 'blur(25px)',
-                        // background: `url(https://m.media-amazon.com/images/I/51dp6BGML8L._SL1280_.jpg)`,
-                        background:  'rgba(139, 172, 170, 0.5)',
-                        border :  '1px solid rgba(255, 255, 255, 0.45) ',   
-                        backgroundSize: 'cover',
-                        
-                        }}  
-                        className=" mx-5 flex-[.8]  md:px-0 px-4 sm:px-24 overflow-y-auto rounded-2xl my-auto py-10 shadow-md shadow-white">
+            <div className="relative flex-row  md:flex md:space-x-5 xl:max-w-3xl lg:max-w-2xl  md:max-w-xl   mx-auto mt-10 ">
+                
 
-                        <div className='flex flex-col '>
-                            <div className=" px-5 flex justify-center  text-xl md:text-2xl font-bold mb-2 leading-tight">
-                                {product.title}
-                            </div>
+                <div  data-aos="fade-right"
+                        className="  items-center justify-center w-full md:w-auto flex md:flex-[.7] max-w-[700px] lg:max-w-full mx-auto lg:mx-0 ">
 
+                                {/*  {image}   */}
+                    <img style={{
+                    
+                    backdropFilter: 'blur(25px)', 
+                    // background:  'rgba(139, 172, 170, 0.5)',
+                    backgroundImage: 'linear-gradient(0deg, rgba(40, 44, 52, 1) 0%, rgba(17, 0, 32, .5) 100%)',
 
-                            {/* PRODUCT RETRIEVW DETAILS */}
-                            <div className="my-5 px-10 md:px-20 lg:px-28 xl:px-36 flex font-semibold justify-between ">
-                                <div>
-                                     {product.rank}
-                                </div>
+                    border :  '1px solid rgba(255, 255, 255, 0.45) ',   
+                    backgroundSize: 'cover', 
+                    }}
+                        src={product.image_url} alt="Product" className="p-5 shadow-md shadow-white w-auto h-100 object-cover  my-4 border-2 rounded-xl" />
 
-                                <div>
-                                    {product.category}
-                                </div>
-                            </div>
+                </div>
+                
+                
+                
+                <div  className="card flex-[0.7] md:flex-[1] border border-gray-400 bg-gray-800 text-white p-8 rounded-lg shadow-white shadow-md">
+                    <div class='main'>
 
-                            {/* PRODUCT PRICE */}
-                            <div className=" flex justify-center  text-base md:text-lg font-bold mb-5">
-                                {product.price}
-                            </div>
-
-
-                            {/* PRODUCT RATINGS */}
-                            <div className="my-5 px-10 md:px-20 lg:px-28 xl:px-36 flex font-semibold justify-between ">
-                                <div>
-                                     {rating}
-
-                                     <Stack spacing={1}> 
-                                     <Rating name="half-rating" defaultValue={rating} precision={0.5} />
-                                    </Stack>
-
-
-                                </div>
-
-                                <div>
-                                    {product.reviews}
-                                </div>
-                            </div>
-
-                             
-
-                              {/* direct to specific website new window when click the button */}
-                            <div className=" flex justify-center font-semibold">
-                                <Button onClick={() => window.open(product.link, '_blank')} className=' rounded-xl bg-cyan-700 py-2 px-3 border border-gray-400'> {product.website}</Button>
-                            </div>
-
-
-
-
+                        <div className="bg    ">
+                            <h1 >{product.price}</h1>
                         </div>
+
+
+                        <div className='flex justify-center  '>
+                           <h2>{product.title}</h2> 
+                        </div>
+
+
+                        <div className="my-5 flex justify-end "> 
+                            <p>{product.category}</p>
+                        </div>  
                         
-  
+                        <p class='mb-7 text-cyan-600'>Evrima will give you nothing,but save your time on us.</p>
+                         
+                        <div class='flex justify-between items-center bg-gray-300 text-gray-900 rounded-md'>
+
+                            {/* ratings */}
+                            <div >   
+                                <div  class="pl-10">
+                                     {rating}
+                                </div>
+                                     <Stack spacing={1}> 
+                                     <Rating name="half-rating-read" defaultValue={rating} precision={0.5} readOnly />
+                                    </Stack> 
+                            </div>
+
+
+                            {/* category */}
+                            <div class="duration"> 
+                                <p>{product.reviews}</p>
+                            </div>
+                        </div>
+                        <hr />
+                         
+
+                        
+                              {/* direct to specific website new window when click the button */}
+                        <div className="mt-5 flex justify-center font-semibold">
+                            <Button onClick={() => window.open(product.link, '_blank')} className=' rounded-xl bg-cyan-700 py-2 px-3 border border-gray-400'> {product.website}</Button>
+                        </div>
+
                     </div>
-                </div> 
-          
+                </div>
+            </div>
+            
         </div>
+
+
+
       
 </div>
    
   
   )
 }
+
+
+ 
